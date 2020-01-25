1aa3ae138aa15c170b019b1213229b46c8be92de
damico williams <damicowilliams@gmail.com>
2020-01-24 19:07:32 -0800

    starting from second axios

--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,16 +1,42 @@
 import React, { useEffect, useState } from "react";
+import axios from "axios";
+import CharacterCard from "./CharacterCard.js";
+import {Link} from "react-router-dom";
+import SearchForm from "./SearchForm";
 
 export default function CharacterList() {
   // TODO: Add useState to track data from useEffect
+  const [character, setCharacter] = useState([])
 
   useEffect(() => {
     // TODO: Add API Request here - must run in `useEffect`
     //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
+    axios.get('https://rickandmortyapi.com/api/character/')
+        .then(response => {
+            console.log(response.data.results);
+            setCharacter(response.data.results);
+        })
+        .catch(err => {
+            console.log(err);
+        })
   }, []);
+
+  const characters = people => {
+    const onSearch = setCharacter([...character, people]);
+  };
 
   return (
     <section className="character-list">
-      <h2>TODO: `array.map()` over your state here!</h2>
+      {character.map(char => (
+        <CharacterCard
+        key={char.id}
+        name={char.name}
+        species={char.species}
+        />
+       
+      ))}
+      <Link to="/">Home</Link>
+      <SearchForm characters={characters}/>
     </section>
   );
 }
--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,9 +1,28 @@
 import React, { useState } from "react";
 
-export default function SearchForm() {
+export default function SearchForm(props) {
+  const [search, setSearch] = useState({ name: "",})
+
+  const handleChange = event => {
+    setSearch({ ...search, [event.target.name]: event.target.value });
+  };
+
+  const handleSubmit = event => {
+    event.preventDefault();
+     const onSearch = {
+       ...search,
+       id: Date.now()
+     };
+    props.characters(onSearch);
+    setSearch({ name: "",});
+  };
  
   return (
     <section className="search-form">
+      <form onSubmit={handleSubmit}>
+        <input onChange={handleChange} type="text" id="" value={search.name}/>
+      </form>
+      <button type="submit">Submit</button>
      // Add a search form here
     </section>
   );
